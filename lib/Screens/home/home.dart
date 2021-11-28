b174ece832ec44e0cc6d27b5898ec8693983ca49
Immani Sri Satya sai <63147179+Satya5122@users.noreply.github.com>
2021-11-28 16:16:38 +0530

    version 4

--- lib/Screens/home/home.dart
+++ lib/Screens/home/home.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: unused_local_variable
 
+import 'dart:async';
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:food_for_the_needy/Services/auth.dart';
+import 'package:food_for_the_needy/Services/database.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -14,19 +19,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var locationMessage = "";
-
   final AuthService _auth = AuthService();
+  late List locations = [];
+  double h = 120;
   User user = FirebaseAuth.instance.currentUser!;
-  List coods = [[], [], [], [], [], []];
-  List locations = [
-    'location1',
-    'location2',
-    'location3',
-    'location4',
-    'location5',
-    'location6'
-  ];
+
   // Future<void> getuserdata() async {
   //   User userdata =
   //   setState(() {
@@ -46,6 +43,12 @@ class _HomeState extends State<Home> {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
     coods = [position.latitude, position.longitude];
+    return coods;
+  }
+
+  List getLocation() {
+    List coods = [];
+    getCurrentLocation().then((value) => {coods = value});
     return coods;
   }
 
@@ -95,10 +98,55 @@ class _HomeState extends State<Home> {
                         longitude = coods[1].toString();
                       });
 
-                      FirebaseFirestore.instance
-                          .collection('LocationDB')
-                          .doc(user.uid)
-                          .set({'latitiude': latitude, 'longitude': longitude});
+                      setState(() {
+                        if (h != 0)
+                          h = 0;
+                        else
+                          h = 120;
+                        locations = [
+                          new LocationData("Location 1", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 2", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 3", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 4", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 5", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 6", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 7", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 8", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 9", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 10", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 11", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 12", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 13", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 14", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 15", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 16", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 17", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 18", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 19", coods[0], coods[1],
+                              17.35846, 38.354455),
+                          new LocationData("Location 20", coods[0], coods[1],
+                              17.35846, 38.354455),
+                        ];
+                        locations.sort((a, b) => a.dist.compareTo(b.dist));
+                      });
                     },
                     child: Container(
                       width: 200,
@@ -118,7 +166,60 @@ class _HomeState extends State<Home> {
                 SizedBox(
                   height: 20,
                 ),
-                Text("$latitude,$longitude")
+                Container(
+                  height: 500,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        ...locations.map((e) => FlatButton(
+                              onPressed: () {
+                                FirebaseFirestore.instance
+                                    .collection('LocationDB')
+                                    .doc(user.uid)
+                                    .set({
+                                  'curr_lat': e.curr_lat.toString(),
+                                  'curr_long': e.curr_lon.toString(),
+                                  'chosen_lat': e.lat,
+                                  'chosen_lon': e.lon,
+                                  'locationName': e.name,
+                                  'uid': user.uid
+                                });
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) =>
+                                        RequestSuccessScreen()));
+                              },
+                              child: Container(
+                                  width: double.infinity,
+                                  height: h,
+                                  child: Card(
+                                    child: Center(
+                                      child: Row(
+                                        children: <Widget>[
+                                          SizedBox(width: 20),
+                                          Text((sqrt((e.curr_lat - e.lat) *
+                                                      (e.curr_lat - e.lat) +
+                                                  (e.curr_lon - e.lon) *
+                                                      (e.curr_lon - e.lon)))
+                                              .toStringAsFixed(0)),
+                                          Text("KM"),
+                                          SizedBox(
+                                            width: 25,
+                                          ),
+                                          Text(
+                                            e.name,
+                                            style: TextStyle(
+                                                fontSize: 30,
+                                                fontFamily: 'OpenSans'),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  )),
+                            ))
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
@@ -126,32 +227,169 @@ class _HomeState extends State<Home> {
   }
 }
 
-class Userdetails extends StatelessWidget {
-  const Userdetails({Key? key}) : super(key: key);
-  void getCurrentLocation() {
-    Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
-        .then((value) {
-      var last_position = Geolocator.getLastKnownPosition();
-      print(last_position);
-      print("$value.longitude ,$value.latitude");
-    });
-  }
+class Userdetails extends StatefulWidget {
+  @override
+  State<Userdetails> createState() => _UserdetailsState();
+}
+
+class _UserdetailsState extends State<Userdetails> {
+  late List Userdata = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
         title: Text("User Info"),
       ),
       body: Container(
-        child: ElevatedButton(
-          onPressed: () {
-            getCurrentLocation();
-            Navigator.of(context).pop();
-          },
-          child: Text("Home"),
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 25),
+          Center(
+            child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(60)),
+                child: Icon(
+                  Icons.person_rounded,
+                  size: 100,
+                )),
+          ),
+          FlatButton(
+              onPressed: () async {
+                //Userdata = _fetch();
+              },
+              child: Text("Fetch Details")),
+          SizedBox(
+            height: 30,
+          ),
+          Column(
+            children: <Widget>[
+              ...(Userdata as List).map((e) => Container(
+                  width: double.infinity,
+                  child: Card(
+                    child: Center(
+                      child: Text(
+                        e,
+                        style: TextStyle(fontSize: 30, fontFamily: 'OpenSans'),
+                      ),
+                    ),
+                  )))
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
+
+const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+Random _rnd = Random();
+
+class RequestSuccessScreen extends StatefulWidget {
+  const RequestSuccessScreen({Key? key}) : super(key: key);
+
+  @override
+  _RequestSuccessScreenState createState() => _RequestSuccessScreenState();
+}
+
+class _RequestSuccessScreenState extends State<RequestSuccessScreen> {
+  static String getRandomString(int length) =>
+      String.fromCharCodes(Iterable.generate(
+          length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+  String code = getRandomString(6);
+  User user = FirebaseAuth.instance.currentUser!;
+  @override
+  Widget build(BuildContext context) {
+    FirebaseFirestore.instance
+        .collection('userCodeDB')
+        .doc(user.uid)
+        .set({'uid': user.uid, 'foodToken': code});
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 50,
+            ),
+            Text("Your food request is succesfully registered"),
+            SizedBox(
+              height: 20,
+            ),
+            Text(code)
+          ],
         ),
       ),
     );
   }
+}
+
+// class MapScreen extends StatefulWidget {
+//   double latitude;
+//   double longitude;
+//   MapScreen(this.latitude, this.longitude);
+
+//   @override
+//   _MapScreenState createState() =>
+//       _MapScreenState(this.latitude, this.longitude);
+// }
+
+// class _MapScreenState extends State<MapScreen> {
+//   double lat;
+//   double lon;
+//   Completer<GoogleMapController> _controller = Completer();
+//   _MapScreenState(this.lat, this.lon);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: GoogleMap(
+//         zoomGesturesEnabled: true,
+//         mapType: MapType.hybrid,
+//         onMapCreated: (GoogleMapController controller) {
+//           _controller.complete(controller);
+//         },
+//         myLocationEnabled: false,
+//         zoomControlsEnabled: true,
+//         initialCameraPosition: CameraPosition(target: LatLng(lat, lon)),
+//       ),
+//     );
+//   }
+// }
+class LocationData {
+  double curr_lat;
+  double curr_lon;
+  double lat;
+  double lon;
+  String name;
+  late double dist;
+  LocationData(this.name, this.curr_lat, this.curr_lon, this.lat, this.lon) {
+    dist = sqrt((curr_lat - lat) * (curr_lat - lat) -
+        (curr_lon - lon) * (curr_lon - lon));
+  }
+}
+
+Future<List> _fetch() async {
+  final firebaseUser = FirebaseAuth.instance.currentUser!;
+  List dataUser = [];
+  if (firebaseUser != null) {
+    FirebaseFirestore.instance
+        .collection('Users')
+        .doc(firebaseUser.uid)
+        .get()
+        .then((ds) {
+      dynamic myEmail = ds.data()!['email'];
+      dynamic name = ds.data()!['name'];
+      dynamic age = ds.data()!['age'];
+      dynamic phone = ds.data()!['phone'];
+      dataUser = [myEmail, name, age, phone];
+    });
+  }
+  return dataUser;
 }
--- lib/Services/database.dart
+++ lib/Services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class databaseService {
   final String? uid;
@@ -11,5 +12,23 @@ class databaseService {
         .collection('Users')
         .doc(uid)
         .set({'name': name, 'age': age, 'phone': phone, 'email': email});
+  }
+
+  Future fetch() async {
+    final firebaseUser = FirebaseAuth.instance.currentUser!;
+    List dataUser = [];
+    if (firebaseUser != null) {
+      FirebaseFirestore.instance.collection('Users').doc(uid).get().then((ds) {
+        dynamic myEmail = ds.data()!['email'];
+        dynamic name = ds.data()!['name'];
+        dynamic age = ds.data()!['age'];
+        dynamic phone = ds.data()!['phone'];
+        dataUser = [myEmail, name, age, phone];
+      }).catchError((e) {
+        print(e);
+      });
+    }
+
+    return dataUser;
   }
 }
